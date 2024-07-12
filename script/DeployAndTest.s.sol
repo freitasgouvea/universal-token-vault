677371a76e3816f3ea65ae8ae2b301122944b53b
Flavio Gouvea <flaviogouveaneto@gmail.com>
2024-07-11 22:59:11 -0300

    chore: fix activate token calls

--- script/DeployAndTest.s.sol
+++ script/DeployAndTest.s.sol
@@ -29,9 +29,9 @@ contract DeployAndTest is Script {
             false, // hasId
             erc20.transferFrom.selector, // deposit function signature
             erc20.transfer.selector, // withdraw function signature
-            1, // amount parameter index for deposit
+            2, // amount parameter index for deposit
+            1, // amount parameter index for withdraw
             0, // id parameter index for deposit (not used)
-            2, // amount parameter index for withdraw
             0  // id parameter index for withdraw (not used)
         );
 
@@ -46,9 +46,9 @@ contract DeployAndTest is Script {
             erc721.transferFrom.selector, // deposit function signature
             erc721.transferFrom.selector, // withdraw function signature
             0, // amount parameter index for deposit (not used)
-            1, // id parameter index for deposit
             0, // amount parameter index for withdraw (not used)
-            1  // id parameter index for withdraw
+            2, // id parameter index for deposit
+            2  // id parameter index for withdraw
         );
 
         // Deploy the test ERC1155 token contract
@@ -62,8 +62,8 @@ contract DeployAndTest is Script {
             erc1155.safeTransferFrom.selector, // deposit function signature
             erc1155.safeTransferFrom.selector, // withdraw function signature
             3, // amount parameter index for deposit
-            2, // id parameter index for deposit
             3, // amount parameter index for withdraw
+            2, // id parameter index for deposit
             2  // id parameter index for withdraw
         );
 
