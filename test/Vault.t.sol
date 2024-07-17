8d88c806c0eb38813be660b0d935d62f233385a2
Flavio Gouvea <flaviogouveaneto@gmail.com>
2024-07-17 00:59:18 -0300

    chore: add withdraw to test vault

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.20;
 
 import { Test, console2 } from "forge-std/Test.sol";
+
+import "forge-std/Test.sol";
+
 import { ERC20Test } from "../src/ERC20Test.sol";
 import {ERC721Test} from "../src/ERC721Test.sol";
 import {ERC1155Test} from "../src/ERC1155Test.sol";
@@ -36,8 +39,8 @@ contract VaultTest is Test, Events {
             erc20.transferFrom.selector, // deposit function signature
             erc20.transfer.selector, // withdraw function signature
             2, // amount parameter index for deposit
-            1, // amount parameter index for withdraw
             0, // id parameter index for deposit (not used)
+            1, // amount parameter index for withdraw
             0  // id parameter index for withdraw (not used)
         );
 
@@ -52,8 +55,8 @@ contract VaultTest is Test, Events {
             erc721.transferFrom.selector, // deposit function signature
             erc721.transferFrom.selector, // withdraw function signature
             0, // amount parameter index for deposit (not used)
-            0, // amount parameter index for withdraw (not used)
             2, // id parameter index for deposit
+            0, // amount parameter index for withdraw (not used)
             2  // id parameter index for withdraw
         );
 
@@ -68,27 +71,26 @@ contract VaultTest is Test, Events {
             erc1155.safeTransferFrom.selector, // deposit function signature
             erc1155.safeTransferFrom.selector, // withdraw function signature
             3, // amount parameter index for deposit
-            3, // amount parameter index for withdraw
             2, // id parameter index for deposit
+            3, // amount parameter index for withdraw
             2  // id parameter index for withdraw
         );
 
+        erc20.mint(alice, UINT256_MAX);
+
+        for (uint i = 1; i < 4; i++) {
+            erc721.mint(alice, i);
+        }
+
+        erc1155.mint(alice, 1, UINT256_MAX, "");
 
         vm.stopPrank();
     }
 
-    function test_Deposit(uint128 amount) public {
+    function test_Deposit(uint256 amount) public {
         if(amount == 0) {
             amount = 100;
         }
-
-        vm.startPrank(owner);
-        erc20.mint(alice, amount);
-        for (uint i = 1; i < 4; i++) {
-            erc721.mint(alice, i);
-        }
-        erc1155.mint(alice, 1, amount, "");
-        vm.stopPrank();
 
         vm.startPrank(alice);
 
@@ -105,7 +107,6 @@ contract VaultTest is Test, Events {
             amount
         );
         vault.deposit(address(erc20), dataERC20);
-
 
         bytes memory dataERC721 = abi.encodeWithSelector(
             erc721.transferFrom.selector, 
@@ -130,10 +131,64 @@ contract VaultTest is Test, Events {
         assertEq(erc20.balanceOf(address(vault)), amount);
         assertEq(vault.getUserTokenBalance(address(alice), address(erc20)), amount);
 
-        assertEq(erc721.balanceOf(address(vault)), 3);
+        assertEq(erc721.balanceOf(address(vault)), 1);
         assertEq(vault.getOwnerOfTokenId(address(erc721), 1), address(alice));
         
         assertEq(erc1155.balanceOf(address(vault), 1), amount);
-        assertEq(vault.getUserBalanceOfTokenId(address(erc1155), address(alice), 1), amount);
+        assertEq(vault.getUserBalanceOfTokenId(address(alice), address(erc1155), 1), amount);
+    }
+
+    function test_Withdraw(uint256 amount) public {
+        if(amount == 0) {
+            amount = 100;
+        }
+
+        test_Deposit(amount);
+
+        vm.startPrank(alice);
+
+        erc20.approve(address(vault), amount);
+        erc721.setApprovalForAll(address(vault), true);
+        erc1155.setApprovalForAll(address(vault), true);
+        
+
+        // Withdraw ERC20
+        bytes memory dataERC20 = abi.encodeWithSelector(
+            ERC20Test(erc20).transfer.selector, 
+            address(alice), 
+            amount
+        );
+        vault.withdraw(address(erc20), dataERC20);
+
+        // Withdraw ERC721
+        bytes memory dataERC721 = abi.encodeWithSelector(
+            erc721.transferFrom.selector, 
+            address(vault), 
+            address(alice), 
+            1
+        );
+        vault.withdraw(address(erc721), dataERC721);
+
+        // Withdraw ERC1155
+        bytes memory dataERC1155 = abi.encodeWithSelector(
+            erc1155.safeTransferFrom.selector, 
+            address(vault), 
+            address(alice), 
+            1, 
+            amount, 
+            ""
+        );
+        vault.withdraw(address(erc1155), dataERC1155);
+
+        vm.stopPrank();
+
+        assertEq(erc20.balanceOf(address(alice)), UINT256_MAX);
+        assertEq(vault.getUserTokenBalance(address(alice), address(erc20)), 0);
+
+        assertEq(erc721.balanceOf(address(alice)), 3);
+        assertEq(vault.getOwnerOfTokenId(address(erc721), 1), address(0));
+        
+        assertEq(erc1155.balanceOf(address(alice), 1), UINT256_MAX);
+        assertEq(vault.getUserBalanceOfTokenId(address(alice), address(erc1155), 1), 0);
     }
 }
